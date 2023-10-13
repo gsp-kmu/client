84d310f8afc73b019d57ad9f2245eca1712b4fbf
sotetero <106152149+SPTETERO@users.noreply.github.com>
2023-10-13 10:27:46 +0900

    main
    main

--- Assets/Scripts/Luna.cs
+++ Assets/Scripts/Luna.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class Luna : Card
 {
+    static GameObject LunaEffect;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        LunaEffect = Resources.Load<GameObject>("Prefebs/Effect/LunaEffect");
     }
 
     // Update is called once per frame
@@ -18,11 +20,63 @@ public class Luna : Card
 
     public override void BattleCry(Digit digit)
     {
+
         GameController controller = GameController.GetInstance();
 
         if (digit == Digit.Ten)
         {
-            controller.player_one.ReceiveCard(this);
+            StartCoroutine(Move(controller.player_ten.transform));
+
         }
+    }
+
+    static IEnumerator Move(Transform start)
+    {
+
+        yield return new WaitForSeconds(0.5f);
+
+        Transform card_transform = start.GetChild(start.childCount - 1).GetComponent<Card>().transform;
+
+        Debug.Log(card_transform);
+
+        Destroy(Instantiate(LunaEffect, card_transform), 2.0f);
+
+
+
+        GameController controller = GameController.GetInstance();
+
+        while (card_transform.localScale != Vector3.one * 1.2f)
+        {
+            card_transform.localScale = Vector3.MoveTowards(card_transform.localScale, Vector3.one * 1.2f, Time.deltaTime * 5);
+
+            yield return new WaitForSeconds(0);
+        }
+
+
+        while (card_transform.localScale != Vector3.zero)
+        {
+            card_transform.localScale = Vector3.MoveTowards(card_transform.localScale, Vector3.zero, Time.deltaTime * 5);
+
+            yield return new WaitForSeconds(0);
+        }
+
+        card_transform.parent = controller.player_one.transform;
+        card_transform.localPosition = Vector3.zero;
+
+        while (card_transform.localScale != Vector3.one * 1.2f)
+        {
+            card_transform.localScale = Vector3.MoveTowards(card_transform.localScale, Vector3.one * 1.2f, Time.deltaTime * 5);
+
+            yield return new WaitForSeconds(0);
+        }
+
+        while (card_transform.localScale != Vector3.one)
+        {
+            card_transform.localScale = Vector3.MoveTowards(card_transform.localScale, Vector3.one, Time.deltaTime * 5);
+
+            yield return new WaitForSeconds(0);
+        }
+
+
     }
 }
