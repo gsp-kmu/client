c522a47e3a79555b030567ce59574ac6a7511325
sotetero <106152149+SOTETERO@users.noreply.github.com>
2023-10-14 00:25:40 +0900

    엘프 메두사 업데이트

--- Assets/Scripts/Elf.cs
+++ Assets/Scripts/Elf.cs
@@ -1,16 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class Elf : Card
 {
-    void Start()
+    static GameObject[] elf_effect;
+    void Awake()
     {
-        
+        elf_effect = new GameObject[3];
+        for(int i = 0; i < elf_effect.Length; i++)
+            elf_effect[i] = Resources.Load<GameObject>("Prefebs/Effect/Elf_effect_" + i.ToString());
     }
 
     void Update()
     {
         
+    }
+    public override void BattleCry(Digit digit)
+    {
+        base.BattleCry(digit);
+
+        GameController controller = GameController.GetInstance();
+        if (digit == Digit.Ten)
+            StartCoroutine(Spawn(controller.player_ten.transform));
+        else
+            StartCoroutine(Spawn(controller.player_one.transform));
+    }
+    public override void BattleCryOpponent(Digit digit)
+    {
+        base.BattleCryOpponent(digit);
+        GameController controller = GameController.GetInstance();
+
+        if (digit == Digit.Ten)
+            StartCoroutine(Spawn(controller.opponent_ten.transform));
+        else
+            StartCoroutine(Spawn(controller.opponent_one.transform));
+    }
+
+    static IEnumerator Spawn(Transform pos)
+    {
+
+        for (int i = 0; i < 20; i++)
+        {
+            GameObject effect = Instantiate(elf_effect[Random.Range(0, 3)], pos);
+            float pos_r = Random.Range(5.0f, 10.0f);
+            float ros_r = Random.Range(0, Mathf.PI * 2);
+
+            effect.transform.localPosition = new Vector3(Mathf.Cos(ros_r), Mathf.Sin(ros_r)) * pos_r;
+            effect.transform.localScale = Vector3.zero;
+            effect.transform.DOScale(Vector3.one * 0.5f, 0.2f);
+            effect.transform.DOPunchRotation(Vector3.forward * 180, 0.7f);
+            effect.GetComponent<SpriteRenderer>().DOFade(0, 1f);
+            Destroy(effect, 1);
+            
+            yield return new WaitForSeconds(0.05f);
+        }
     }
 }
---
+++ Assets/Scripts/Medusa.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Medusa : Card
+{
+    static GameObject effect;
+
+    void Start()
+    {
+        effect = Resources.Load<GameObject>("Prefebs/Effect/Medusa_effect");
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public override void BattleCry(Digit digit)
+    {
+        base.BattleCry(digit);
+
+        StartCoroutine(Stun(transform));
+    }
+
+    static IEnumerator Stun(Transform tf)
+    {
+        yield return new WaitForSeconds(0.5f);
+        GameObject effect_obj = Instantiate(effect, tf);
+        Image effect_img = effect_obj.GetComponentInChildren<Image>();
+        effect_img.DOFade(0.8f, 0.3f);
+        yield return new WaitForSeconds(0.3f);
+        effect_img.DOFade(0f, 0.3f);
+        yield return new WaitForSeconds(0.3f);
+    }
+}
