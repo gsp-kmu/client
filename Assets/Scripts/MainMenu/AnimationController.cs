4721703a5759c94b7354e28675285ce895058c81
mclub4 <mclub4@kookmin.ac.kr>
2023-11-18 03:17:40 +0900

    로그인 UI 개선/메인화면 랜덤

--- Assets/Scripts/MainMenu/AnimationController.cs
+++ Assets/Scripts/MainMenu/AnimationController.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class AnimationController : MonoBehaviour
 {
@@ -15,9 +16,34 @@ public class AnimationController : MonoBehaviour
     public Vector3 rotate2;
     public float rotateTime1;
     public float rotateTime2;
+
+    public Image characterImage1;
+    public Image characterImage2;
+    public Image background;
+
+    public Sprite[] characterSprites1;
+    public Sprite[] characterSprites2;
+    public Sprite[] backgroundSprites;
+
     // Start is called before the first frame update
     void Start()
     {
+        int idx1 = Random.Range(0, characterSprites1.Length);
+        int idx2 = Random.Range(0, characterSprites2.Length);
+        Sprite randomCharacter1 = characterSprites1[idx1];
+        Sprite randomCharacter2 = characterSprites2[idx2];
+        Sprite randomBackground = backgroundSprites[Random.Range(0, backgroundSprites.Length)];
+
+        characterImage1.sprite = randomCharacter1;
+        characterImage2.sprite = randomCharacter2;
+        background.sprite = randomBackground;
+
+        if(idx1 == 1 || idx1 == 2)
+        {
+            characterImage1.transform.localScale = new Vector3((float)7.622005, (float)9.087746, (float)3.709587);
+        }
+        
+
         StartCoroutine(Moveit());
     }
 
