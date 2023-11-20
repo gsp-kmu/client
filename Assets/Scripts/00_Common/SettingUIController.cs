abb88725fd68ac91504675b0fe6c3e0900eccd5d
hyunseo <hansx2079@naver.com>
2023-11-20 20:44:35 +0900

    설정 ui 완성

--- Assets/Scripts/00_Common/SettingUIController.cs
+++ Assets/Scripts/00_Common/SettingUIController.cs
@@ -7,12 +7,25 @@ public class SettingUIController : MonoBehaviour
 {
     public Toggle bgmToggle;
     public Toggle sfxToggle;
+    public Button LogoutButton;
+    public Button UIOnButton;
+    public Button CloseButton;
 
     private void Start() {
         bgmToggle.isOn = SettingService.Instance.GetMuteBGM();
         sfxToggle.isOn = SettingService.Instance.GetMuteSFX();
         bgmToggle.onValueChanged.AddListener(ClickBGMToggle);
         sfxToggle.onValueChanged.AddListener(ClickSFXToggle);
+        LogoutButton.onClick.AddListener(Logout);
+
+        UIOnButton.onClick.AddListener(() =>
+        {
+            gameObject.SetActive(true);
+        });
+        CloseButton.onClick.AddListener(() =>
+        {
+            gameObject.SetActive(false);
+        });
     }
 
     public void ClickBGMToggle(bool isOn){
@@ -21,6 +34,11 @@ public class SettingUIController : MonoBehaviour
 
     public void ClickSFXToggle(bool isOn){
         SettingService.Instance.SetMuteSFX(isOn);
+    }
+
+    public void Logout()
+    {
+        NetworkService.Instance.io.Disconnect();
     }
 
 }
